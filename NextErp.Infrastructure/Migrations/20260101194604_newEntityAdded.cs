536c9e49194709abf40cfabf5c5425712ca0b1ef
rifatiyaan <mdbrifat@gmail.com>
2026-01-04 01:06:06 +0600

    Updated Configuration

---
+++ NextErp.Infrastructure/Migrations/20260101194604_newEntityAdded.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NextErp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class newEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Empty migration part to fix orphan designer file
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Empty migration part to fix orphan designer file
+        }
+    }
+}
